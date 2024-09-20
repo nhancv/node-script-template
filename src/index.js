4c78c625cc1193b6ee94fbb00fda7c328c4cb1cc
Nhan Cao <nhancv92@gmail.com>
2024-09-20 17:44:46 +0700

    feat: update template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 
-const { sleepWithProgress } = require('./libs/utils');
+const { sleep } = require('./libs/utils');
 
 const processScript = async () => {
-  await sleepWithProgress(1000);
+  await sleep(1000);
 };
 
 processScript()
-  .then(() => {
-    console.log('DONE');
-  })
-  .catch((error) => console.error(error));
+  .catch((error) => console.error(error))
+  .finally(() => console.log('Exiting...'));
